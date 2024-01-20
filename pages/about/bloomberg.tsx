e2d248aec48a4433931343fe142370ccbb6e65a1
Tanson Lee <leetanson@gmail.com>
2024-01-19 22:54:46 -0500

    spacing

--- pages/about/bloomberg.tsx
+++ pages/about/bloomberg.tsx
@@ -9,16 +9,18 @@ const GlobalIllumination = () => {
 			date='Sept - Dec 2023'
 			imagePath='/images/bloomberg.JPG'
 		>
-			<Text>
-				At Bloomberg, I worked on the Fast Pricing Infrastructure team. Our team builds the
-				infrastructure layer for performing pricing and risk analytics on potentially
-				massive portfolios. I learned a lot about software architecture design and
-				distributed systems throughout the term.
-			</Text>
-			<Text>
-				I worked primarily in C++ using technologies such as gRPC, Kubernetes, and
-				Cassandra.
-			</Text>
+			<Stack spacing={8}>
+				<Text>
+					At Bloomberg, I worked on the Fast Pricing Infrastructure team. Our team builds
+					the infrastructure layer for performing pricing and risk analytics on
+					potentially massive portfolios.
+				</Text>
+				<Text>
+					I learned a lot about software architecture design and distributed systems
+					throughout the term. I worked primarily in C++ using technologies such as gRPC,
+					Kubernetes, and Cassandra.
+				</Text>
+			</Stack>
 		</AboutPageLayout>
 	);
 };
